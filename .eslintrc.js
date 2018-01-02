6b74562d23ac7e5632fd2b34928a38a8ff1feba5
joseph bartels <joebartels@hotmail.com>
2018-01-02 15:59:24 -0500

    initial commit. working model w/ tests

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended'
+ ],
+  env: {
+    'browser': false,
+    'node': true
+  },
+  globals: {
+
+  },
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
+    'space-before-function-paren': ['error', { 'anonymous': 'never', 'named': 'never' }],
+    'indent': ['error', 2, {
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3
+      },
+      'SwitchCase': 1,
+      'MemberExpression': 'off'
+    }],
+    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+    'max-statements-per-line': [2, { 'max': 2 }],
+    'operator-linebreak': ['error', 'after'],
+    'new-cap': ['error', {
+      'capIsNewExceptions': [
+        'Ember$',
+        'A',
+        'EmberObject',
+        'Event',
+        'Velocity',
+        'Logger',
+        'EmberSet',
+        'Stripe'
+      ],
+      'newIsCapExceptions': ['defer']
+    }],
+    'camelcase': 'error',
+    'no-eval': 'error',
+    'one-var': 'error'
+  }
+};
---
+++ tests/test-runner.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const glob  = require('glob');
+const Mocha = require('mocha');
+
+const arg = process.argv[2];
+const grep = process.argv[3];
+const root = 'lib';
+
+const mochaOptions = {
+  retport: 'spec'
+};
+
+if (grep) {
+  mochaOptions.grep = grep;
+}
+
+const mocha = new Mocha(mochaOptions);
+
+function addFiles(mocha, files) {
+  console.log(`testing file paths: ${files}`);
+
+  glob.sync(files).forEach(mocha.addFile.bind(mocha));
+}
+
+if (arg === 'all') {
+  addFiles(mocha, `${root}/**/*-test.js`);
+  addFiles(mocha, `${root}/**/*-slow.js`);
+} else if (arg) {
+  addFiles(mocha, arg);
+} else {
+  addFiles(mocha, `${root}/**/*-test.js`);
+}
+
+mocha.run(function(failures) {
+  process.on('exit', function() {
+    process.exit(failures);
+  });
+});
+
+// TODO:CLEANUP
+// be smart about testing directories via CLI arg. e.g.,
+// npm test tests/transforms
+// => glob.sync(tests/transforms/**/**-test.js)
+//
+//    Check if directory, and apply the wildcard glob
+//      or
+//    Apply wildcard glob when no file extension exists
